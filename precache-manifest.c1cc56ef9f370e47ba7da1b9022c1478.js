0947cf158acc714feecdc33475f580ae3778ca13
Dylan Perkins <dyllper@gmail.com>
2019-10-30 21:35:21 -0500

    Updates

---
+++ precache-manifest.c1cc56ef9f370e47ba7da1b9022c1478.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2482dc8be31a18ed211878f67c27418c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "30f422cb8b1d1b5067f5",
+    "url": "/monsters-rolodex/static/css/main.8f4327c9.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/monsters-rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "30f422cb8b1d1b5067f5",
+    "url": "/monsters-rolodex/static/js/main.30aa4c64.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
